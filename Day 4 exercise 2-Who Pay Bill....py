e8c9219deef103088ce8504dd653d6d9160eb94a
Jorge96273 <115398843+Jorge96273@users.noreply.github.com>
2022-10-11 16:41:54 +0900

    Add files via upload

---
+++ Day 4 exercise 2-Who Pay Bill....py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+# 78 65 89 86 55 91 64 89
+#Write your code below this row 👇
+highest_score = 0
+for highest_score in student_scores:
+  highest_score < n
+  highest_score > n 
+
+print(f"The highest score in the class is: {highest_score}")
